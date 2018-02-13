5ef2bc4011510050ad5602cfb66431e4393ae929
James McDaniel <icathaid@gmail.com>
2018-02-12 18:27:04 -0800

    committing basic JS functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
-alert('stuff');
 var storeHours = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM'];
 var pike = {
-  name: 'Pike',
+  name: 'pike',
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
@@ -11,10 +10,20 @@ var pike = {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) +min);
   },
-}
-  console.log('1st and Pike');
-  for (var i = 0; i < storeHours.length; i++){
-   var temp = Math.floor((pike.rate(pike.minCust, pike.maxCust) * pike.avgCookies));
-   pike.sales.push(temp);
-   console.log(storeHours[i] + ': ' + pike.sales[i])
-}
+  pushSales: function() {
+    for (var i = 0; i < storeHours.length; i++){
+      var temp = Math.floor((pike.rate(pike.minCust, pike.maxCust) * pike.avgCookies));
+      pike.sales.push(temp);
+      }
+    },
+  render: function() {
+    var ulEl = document.getElementById('pike');
+      for (var i = 0; i < storeHours.length; i++) {
+        var liEl = document.createElement('li');
+        liEl.textContent = storeHours[i] + ': ' + pike.sales[i];
+        ulEl.appendChild(liEl);
+      }
+    },
+};
+//pike.pushSales();
+pike.render();
