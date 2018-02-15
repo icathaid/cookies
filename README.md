f7390baec40000b61978c1b82f76a956ab330cb4
James McDaniel <icathaid@gmail.com>
2018-02-14 16:37:20 -0800

    fixed js/html

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cookies
+
+I keep forgetting to update the readme.
+And to learn markdown.
+
+Also I made all of today's edits on master.  Woops.
+
+Anyway, the JS and HTML are now working, so I'm committing changes to remote:master and I'll continue styling the CSS later.  Hopefully on its own branch.
--- js/app3.js
+++ js/app3.js
@@ -1,9 +1,8 @@
-var storeHours = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM'];
+var storeHours = ['6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00'];
 var patStores = [];
 var storeTable = document.getElementById('pike'); 
-function Store(id, name, minCust, maxCust, avgCookies) {
-    this.id = id;
-    this.name = name;
+function Store(name, minCust, maxCust, avgCookies) {
+    this.name = name + ' sales: ';
     this.minCust = minCust;
     this.maxCust = maxCust;
     this.avgCookies = avgCookies;
@@ -12,14 +11,12 @@ function Store(id, name, minCust, maxCust, avgCookies) {
     this.dailySales = [];
     this.calcDailySales();
     patStores.push(this);
-    console.table(this);
-    this.render();
 }
 Store.prototype.calcHourlySales = function(){
-    for (var i = 0; i < storeHours.length; i++){
+    for (var i = 0; i <= storeHours.length; i++){
         this.hourlySales.push(
             Math.floor((Math.random() * (this.maxCust-this.minCust)) + this.minCust))
-        };
+    };
 };
 Store.prototype.calcDailySales = function(){
     var total = 0;
@@ -30,24 +27,39 @@ Store.prototype.calcDailySales = function(){
 };
 Store.prototype.render = function(){
     var trEl = document.createElement('tr');
-    var tdEl = document.createElement('td');
+    var tdEl = document.createElement('td');	
     tdEl.textContent = this.name;
     trEl.appendChild(tdEl);
+    for (var i = 0; i < storeHours.length; i++){
+        var tdEl = document.createElement('td');
+        tdEl.textContent = this.hourlySales[i];
+        trEl.appendChild(tdEl);
+    };
+    storeTable.appendChild(trEl);
 };
-new Store('pike', 'pike', 23, 65, 6.3);
-new Store('seatac', 'seatac', 11, 19, 2.2);
-    
-  /* Store.prototype.render = function() {
-    var ulEl = document.getElementById(this.id);
-    this.calcHourly();
-      for (var i = 0; i < storeHours.length; i++) {
-        var liEl = document.createElement('li');
-        liEl.textContent = storeHours[i] + ': ' + this.hourlySales[i];
-        ulEl.appendChild(liEl);
-      } 
-    var ulEl = document.getElementById(this.id)
-      liEl.textContent = 'Daily Sales: ' + this.dailySales;
-      ulEl.appendChild(liEl);
-      console.log(this.dailySales);
-    },
-    */
+new Store('pike', 23, 65, 6.3);
+new Store('seatac', 11, 19, 2.2);
+new Store('sc', 11, 38, 3.7);
+new Store('capHill', 20, 38, 2.3);
+new Store('alki', 2, 16, 4.6);
+//-=-=-=-=-=-=-
+function makeHeaderRow(){
+    //var trEl = document.createElement('tr');
+    var trEl = document.createElement('tr');
+    var thEl = document.createElement('th');
+    thEl.textContent = 'Name: ';
+    trEl.appendChild(thEl);
+    for (var i = 0; i < storeHours.length; i++){
+        var thEl = document.createElement('td')
+        thEl.textContent = storeHours[i];
+        trEl.appendChild(thEl);
+        storeTable.appendChild(trEl);
+    }
+};
+function makeStoreRows(){
+    for (var i = 0; i < storeHours.length; i++){
+        patStores[i].render();
+    };
+};
+makeHeaderRow();
+makeStoreRows();
